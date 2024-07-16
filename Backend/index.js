63e18b0cea37d0bd0cde9091265644e2615dbdf2
kavishka <thushalrasingolla99@gmail.com>
2024-07-16 14:23:19 +0530

    backend initiated

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import cookieparser from 'cookie-parser'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+dotenv.config()
+
+const app=express()
+
+app.use(cors())
+app.use(cookieparser())
+app.use(express.json())
+
+mongoose.connect(process.env.DATABASE_URL).then(()=>(
+    console.log('Database connected successfully')
+)).catch((error)=>(console.log('Database connected unsuccessfully',error)))
+
+const port=process.env.PORT || 3000
+
+app.listen(port,()=>(console.log(`Server is running on ${port}`)))
